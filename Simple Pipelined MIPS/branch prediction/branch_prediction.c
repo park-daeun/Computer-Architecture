3dde2785b8bf141b08675e22d5e420b227b59cf9
park-daeun <70877497+park-daeun@users.noreply.github.com>
2020-12-20 13:39:56 +0900

    Create branch_prediction.c

---
+++ Simple Pipelined MIPS/branch prediction/branch_prediction.c
@@ -0,0 +1,636 @@
+
+ /*
+
+   32171550 박다은
+
+   Mobile Processor
+   Simple Pipelined MIPS
+   project #3
+
+   BRANCH PREDICTION
+
+*/
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include "INST.h"
+
+struct predict{
+	int target[10];
+	int BTB_PC[10];
+	int BTB_index;
+	int index;
+	int valid;
+}p;
+
+///micro-architectural state
+int Memory[0x1000000/4];
+int PC;
+int Regs[32];
+
+//count
+int cycle = 0;
+int inst_count = 0;
+int m_count = 0;
+int r_count = 0;
+int b_count = 0;
+int nb_count = 0;
+int j_count = 0;
+
+//control signal in Decode stage
+struct con_signal d_c;
+
+//five stages latches
+struct IFL if_latch[2];
+struct IDL id_latch[2];
+struct EXL ex_latch[2];
+struct MML mm_latch[2];
+struct WBL wb_latch[2];
+
+//stages 시작 전
+void load_memory(FILE* fp);//file 읽고 메모리 load
+void machine_initialization();//레지스터 값 초기화
+
+void control(int op, int fun, int c_rs, int c_rt);//control type별 구분
+
+//five stages(pipeline stages)
+int fetch(struct IFL *if_out, struct IFL *if_in, struct IDL *id_in, struct EXL *ex_in);
+int decode(struct IFL *if_in, struct IDL *id_out, struct MML *mm_in);
+int execute(struct IDL *id_in, struct EXL *ex_out, struct EXL *ex_in, struct MML *mm_in, struct WBL *wb_in);
+int memory(struct EXL *ex_in, struct MML *mm_out, struct MML *mm_in);
+int write_back(struct MML *mm_in, struct WBL *wb_out);
+
+//cycle 맨 마지막에서 print changed state, count instructions, update pipeline
+void state_and_update_cnt(struct IFL if_latch[2], struct IDL id_latch[2], struct EXL ex_latch[2], struct MML mm_latch[2], struct WBL wb_latch[2]);
+
+//모든 cycle이 종료되고 count한 값들 출력
+void print_stastics();
+
+int main(int argc, char* argv[]){
+	//file
+	FILE *fp = NULL;
+
+	if(argc == 2)//2번 째 자리에 파일명 입력하면 그 파일을 읽음
+		fp = fopen(argv[1], "rb");//"rb" : 읽기모드+이진파일모드
+	if(fp == NULL){//입력하지 않았거나 잘못 입력했으면 오류 표시
+		perror("no such input file)");//오류메세지 출력 함수
+		return 0;//프로그램 종료
+	}
+
+	load_memory(fp);//파일의 명령어들을 memory에 load
+	fclose(fp);//파일 닫기
+
+	machine_initialization();//레지스터 값 초기화
+
+	int ret = 0;//return value
+	while(1){
+		//cycle 수 세기
+		cycle++;
+		printf("\n-------------cycle %d----------------\n",cycle);
+
+		//five stages
+		//ret = write_back(&mm_latch[0], &wb_latch[0]);
+		//if(ret<0) break;
+		ret = fetch(&if_latch[0],&if_latch[1],&id_latch[1],&ex_latch[1]);
+		ret = decode(&if_latch[1],&id_latch[0],&mm_latch[1]);
+		ret = execute(&id_latch[1],&ex_latch[0],&ex_latch[1],&mm_latch[1],&wb_latch[1]);
+		if(ret<0) break;
+		ret = memory(&ex_latch[1],&mm_latch[0],&mm_latch[1]);
+		ret = write_back(&mm_latch[1],&wb_latch[0]);
+		if(ret<0) break;
+
+		//print changed state, count instructions, update pipeline
+		state_and_update_cnt(if_latch, id_latch, ex_latch, mm_latch, wb_latch);
+	}
+
+	print_stastics();//count한 값들 출력
+
+	return 0;
+}
+
+////file 읽고 메모리 load
+void load_memory(FILE* fp){
+	int rev_inst = 0;
+	int index = 0;
+	int data = 0;
+	size_t ret = 0;
+
+	//파일의 명령어들이 뒤집혀 있기 때문에 원하는 명령어로 바꾼뒤에 메모리에 업로드해준다.
+	while(1){
+		int h1 = 0;
+		int h2 = 0;
+		int h3 = 0;
+		int h4 = 0;
+		int rev_inst = 0;
+
+		//reading file to eof
+		ret = fread(&data, sizeof(data), 1, fp);
+		if(ret == 0) break;
+
+		h1 = ((data & 0xff) << 24);
+                h2 = ((data & 0xff00) << 8);
+                h3 = ((data & 0xff0000) >> 8) & 0xff00;
+                h4 = ((data & 0xff000000) >> 24) & 0xff;
+
+                rev_inst = h1 | h2 | h3 | h4;
+
+		Memory[index/4] = rev_inst;
+                printf("Load Memory[%08x] val:0x%08x\n", index, Memory[index/4]);
+                index = index+4;
+	}
+}
+
+//레지스터 값 초기화
+void machine_initialization(){
+	Regs[31] = 0xffffffff;
+	Regs[29] = 0x100000;
+}
+
+
+//five stages(pipeline stages)
+//1.fetch -> 2.decode -> 3.execution -> 4.memory -> 5.write back
+
+
+//1.fetch
+int fetch(struct IFL *if_out, struct IFL *if_in, struct IDL *id_in, struct EXL *ex_in){
+	if(PC == 0xffffffff){
+                if_out->pc4 = 0xffffffff;
+                if_out->ir = 0x0;
+                //if_out->valid = 0;
+                return 0;
+        }
+
+	if_out->inst = Memory[PC/4];
+
+        if_out->valid = 1;
+        if_out->ir = if_out->inst;
+        if_out->pc4 = PC;
+
+	//branch 검색		
+	for(int i=0; i<10; i++){
+		if(PC == p.BTB_PC[i] && PC != 0){
+			if_out->npc = p.target[i];
+			if(p.BTB_PC[i] == p.target[i]){
+				if_out->npc = PC + 4;
+			}
+			break;
+		}
+		else{
+			if_out->npc = PC+4;
+		}
+	}
+	PC = if_out->npc;
+
+	if(id_in->valid == 1 && id_in->npc){
+		//jump할 address로 PC값을 바꾼다
+		PC = id_in->npc;
+		if(PC == 0xffffffff){
+			if_out->pc4 = 0xffffffff;
+			if_out->ir = 0x0;
+			return 0;
+		}
+		if_out->inst = Memory[PC/4];
+		if_out->ir = if_out->inst;
+		if_out->npc = PC+4;
+	}
+	
+	/*
+	//fetch 단계 printf
+	printf("[F] inst: 0x%08x, PC:0x%x\n", if_out->ir, if_out->pc4);
+	printf("	M[0x%x] 0x%08x\n", PC, if_out->ir);
+	*/
+
+	//fetch된 명령어들의 개수를 센다
+	inst_count++;
+	return 0;
+}
+
+//2.decode
+int decode(struct IFL *if_in, struct IDL *id_out, struct MML *mm_in){
+	if(if_in->valid == 0) return 0;
+
+	int inst = if_in->inst;
+
+	struct instruction D_inst;
+	D_inst.opcode = (inst & 0xfc000000) >> 26;
+        D_inst.rs     = (inst & 0x03e00000) >> 21;
+        D_inst.rt     = (inst & 0x001f0000) >> 16;
+        D_inst.rd     = (inst & 0x0000f800) >> 11;
+        D_inst.shamt  = (inst & 0x000007c0) >> 6;
+        D_inst.func   = (inst & 0x0000003f) >> 0;
+        D_inst.imm    = (inst & 0x0000ffff) >> 0;
+        D_inst.addr   = (inst & 0x03ffffff) >> 0;
+        D_inst.simm   = (D_inst.imm >> 15) ?
+                (0xffff0000 | D_inst.imm) : D_inst.imm;
+        D_inst.zimm   = D_inst.imm;
+        D_inst.Br_Addr= D_inst.simm << 2;
+        D_inst.J_Addr = ((if_in->pc4 & 0xf0000000) | D_inst.addr) << 2;
+
+	id_out->i = D_inst;
+
+	/*
+	//decode 단계 printf
+	printf("[D] inst: 0x%08x, PC:0x%0x\n", if_in->ir, if_in->pc4);
+	printf("	opcode: 0x%x, rs:%d, rt:%d, rd:%d, shamt:%d, func:0x%x\n",
+			D_inst.opcode, D_inst.rs, D_inst.rt, D_inst.rd, D_inst.shamt, D_inst.func);
+	printf("	simm: 0x%x\n", D_inst.simm);
+	*/
+
+	//control type별 구분
+	control(D_inst.opcode, D_inst.func, D_inst.rs, D_inst.rt);
+	id_out->c = d_c;
+
+	//execute의 ALU에서 사용할 v1,v2 정하기
+	//v1은 모두 rs
+	id_out->v1 = Regs[id_out->i.rs];
+
+	if(id_out->c.ALUSrc) //ALUSrc = 1이면(I-type) v2 = simm
+		id_out->v2 = id_out->i.simm;
+	else //ALUSrc = 0이면(R-type) v2 = rt
+		id_out->v2 = Regs[id_out->i.rt];
+
+	//zimm, imm, shamt 사용하는 것들 예외처리
+	if(id_out->i.opcode == 0xc || id_out->i.opcode == 0xd)
+		id_out->v2 = id_out->i.zimm;
+	if(id_out->i.opcode == 0xf)
+		id_out->v2 = id_out->i.imm;
+	if(id_out->i.opcode == 0 && (id_out->i.func == 0x0 || id_out->i.func == 0x2))
+		id_out->v1 = id_out->i.shamt;
+
+	//WB단계에서 Write할 Register 정하기
+	if(id_out->c.RegDst) //RegDst = 1이면(R-type) WReg = rd
+		id_out->WReg = id_out->i.rd;
+	else //RegDst = 0이면(I-type) WReg = rt
+		id_out->WReg = id_out->i.rt;
+
+	//PC JUMP--------------------------------------------------
+	int ToJump = Regs[id_out->i.rs];//R[rs]로 점프(jr,jalr에서만)
+
+	//forwarding
+	if(mm_in->c.RegWrite) //R-type, I-type, LW
+		if(id_out->i.rs == mm_in->WReg)
+			ToJump = mm_in->mm_output;
+
+	if(id_out->c.Jump){
+		if(id_out->i.opcode == 0 && id_out->i.func == 0x8) //jr
+			PC = ToJump;
+		else if(id_out->i.opcode == 0 && id_out->i.func == 0x9){ //jalr
+			Regs[id_out->i.rd] = if_in->npc + 4;
+			PC = ToJump;
+		}
+		else if(id_out->i.opcode == 0x2) //j
+			PC = id_out->i.J_Addr;
+		else if(id_out->i.opcode == 0x3){ //jal
+			Regs[31] = if_in->npc + 4;
+			PC = id_out->i.J_Addr;
+		}
+		//printf("\t\t\tJump! ~> 0x%x\n",PC);
+	}
+	else
+		id_out->npc = 0;
+
+	//이전 cycle fetch의 output을 현재 cycle decode에서 사용
+        id_out->valid = if_in->valid;
+        id_out->ir = if_in->ir;
+	id_out->pc4 = if_in->pc4;
+
+	return 0;
+}
+
+//3. execute
+int execute(struct IDL *id_in, struct EXL *ex_out, struct EXL *ex_in, struct MML *mm_in, struct WBL *wb_in){
+	if(id_in->valid == 0) return 0;
+
+	int v1, v2, ALU_result;
+	int bcond = 0; //branch taken or not taken 구분
+
+	v1 = id_in->v1;
+	v2 = id_in->v2;
+
+	//forwarding
+	if(wb_in->c.RegWrite){ //R-type, I-type, LW
+		if(id_in->i.rs == wb_in->WReg)
+			v1 = wb_in->wb_v;
+		if(id_in->i.rt == wb_in->WReg)
+			if(!(id_in->c.ALUSrc)) //I-type, LW //없으면 오류남
+				v2 = wb_in->wb_v;
+	}
+	if(mm_in->c.RegWrite){ //R-type, I-type, LW
+		if(id_in->i.rs == mm_in->WReg)
+			v1 = mm_in->mm_output;
+		if(id_in->i.rt == mm_in->WReg)
+			if(!(id_in->c.ALUSrc)) //I-type, LW
+				v2 = mm_in->mm_output;
+	}
+	if(ex_in->c.RegWrite){ //R-type, I-type, LW
+                if(id_in->i.rs == ex_in->WReg){
+                        v1 = ex_in->ALU_result;
+                        if(ex_in->i.opcode == 0x23)
+                                v1 = Memory[ex_in->ALU_result];
+                }
+                if(id_in->i.rt == ex_in->WReg)
+                        if(!(id_in->c.ALUSrc)){ //I-type, LW
+                                v2 = ex_in->ALU_result;
+                                if(ex_in->i.opcode == 0x23)
+                                v2 = Memory[ex_in->ALU_result];
+                        }
+        }
+
+	//forwarding에서 v1값 업데이트됨(v1이 shamt라 업데이트되면 안됨)
+	if(id_in->i.opcode == 0 && (id_in->i.func == 0x0 || id_in->i.func == 0x2))
+		v1 = id_in->i.shamt;
+
+	switch(id_in->c.ALUOp){
+		case 0x100: //j, jal, jalr, jr
+                        break;
+                case 0x20: //add //+sw, lw
+                case 0x21: //addu
+                case 0x8: //addi
+                case 0x9: //addiu
+                        ALU_result = v1 + v2;
+                        break;
+                case 0x24: //and
+                case 0xc: //andi
+                        ALU_result = v1 & v2;
+                        break;
+                case 0xf: //lui
+                        ALU_result = v2 << 16;
+                        break;
+                case 0x27: //nor
+                        ALU_result = ~(v1 | v2);
+                        break;
+                case 0x25: //or
+                case 0xd: //ori
+                        ALU_result = v1 | v2;
+                        break;
+                case 0x2a: //slt
+                case 0x2b: //sltu
+                case 0xa: //slti
+                        ALU_result = (v1 < v2) ? 1:0;
+                        break;
+                case 0x2: //srl
+                        ALU_result = v2 >> v1;
+                        break;
+                case 0x22: //sub
+                case 0x23: //subu
+                        ALU_result = v1 - v2;
+                        break;
+                case 0x4: //beq
+                        if(v1 == v2)
+                                bcond = 1;
+                        break;
+                case 0x5: //bne
+                        if(v1 != v2)
+                                bcond = 1;
+                        break;
+		case 0x0: //sll
+                        ALU_result = v2 << v1;
+                        break;
+                default: //잘못된 instruction 명령 시 프로그램 종료
+                        printf("\twrong insrtuction\n");
+                        return -1;
+        }
+
+	/*
+	//execute 단계 printf
+	printf("[E] inst: 0x%08x, PC: 0x%x\n", id_in->ir, id_in->pc4);
+	printf("	ALU result: 0x%x\n", ALU_result);
+	*/
+
+	ex_out->ALU_result = ALU_result;
+	ex_out->bcond = bcond;
+
+	if(id_in->c.Branch){
+		if(bcond){
+			ex_out->npc = id_in->pc4 + 4 + id_in->i.Br_Addr;
+			printf("%x\n",ex_out->npc);
+		}
+		else
+			ex_out->npc = id_in->pc4;
+
+		//branch 검색
+		for(int i = 0 ; i<10 ; i++){
+                        if(p.BTB_PC[i] == id_in->pc4){
+                                p.BTB_index = i;
+                                p.valid = 1;
+                                break;
+                        }
+                        else
+                                p.valid = 0;
+                }
+
+		if(p.valid == 1){
+			if(p.target[p.BTB_index] != ex_out->npc){
+				PC = ex_out->npc;
+				memset(&if_latch[0], 0 ,sizeof(if_latch));
+				memset(&id_latch[0], 0 ,sizeof(id_latch));
+				p.target[p.BTB_index] = ex_out->npc;
+			} 
+		}
+		else{
+			if(bcond)
+				if_latch->valid = 0;
+			PC = ex_out->npc;
+			p.target[p.index] = PC;
+			p.BTB_PC[p.index] = id_in->pc4;
+			//memset(&if_latch[0], 0 , sizeof(if_latch));
+			p.index++;
+		}		
+        }
+
+        ex_out->valid = id_in->valid;
+        ex_out->ir = id_in->ir;
+        ex_out->i = id_in->i;
+        ex_out->c = id_in->c;
+        ex_out->pc4 = id_in->pc4;
+        ex_out->v1 = id_in->v1;
+        ex_out->v2 = id_in->v2;
+        ex_out->WReg = id_in->WReg;	
+
+	return 0;
+}
+
+//4. memory
+int memory(struct EXL *ex_in, struct MML *mm_out, struct MML *mm_in){
+	if(ex_in->valid == 0) return 0;
+
+	int Address, v2;
+
+	//memory 단계printf
+	//printf("[M] inst: 0x%08x, PC: 0x%x\n", ex_in->ir, ex_in->pc4);
+
+	//LW
+	if(ex_in->c.MemRead){ //메모리 가져오기
+		Address = Memory[ex_in->ALU_result];
+		//printf("        Read Memory\n");
+                //printf("                0x%x <= M[0x%x]\n",
+                //                Address, ex_in->ALU_result);
+                mm_out->mm_output = Address;
+	}
+
+	//SW
+	else if(ex_in->c.MemWrite){ //메모리 저장하기
+		v2 = Regs[ex_in->i.rt];
+		
+	//forwarding
+	if(mm_in->c.RegWrite) //R-type, I-type, LW
+		if(ex_in->i.rt == mm_in->WReg)
+			v2 = mm_in->mm_output;
+
+	Memory[ex_in->ALU_result] = v2;
+	//printf("        Write Memory\n");
+	//printf("                M[0x%x] <= R[%d](0x%x)\n",
+	//		ex_in->ALU_result, ex_in->i.rt, v2);
+}
+
+//jal, jalr (decode단계 Jump에서 사용)
+else if(ex_in->i.opcode == 3 || (ex_in->i.opcode == 0 && ex_in->i.func == 9))
+	mm_out->mm_output = ex_in->pc4;
+else //ex단계 forwarding에서 사용
+		mm_out->mm_output = ex_in->ALU_result;
+
+        mm_out->valid = ex_in->valid;
+        mm_out->ir = ex_in->ir;
+        mm_out->pc4 = ex_in->pc4;
+        mm_out->i = ex_in->i;
+        mm_out->c = ex_in->c;
+        mm_out->WReg = ex_in->WReg;
+        mm_out->ALU_result = ex_in->ALU_result;	
+
+	return 0;
+}
+
+//5. write back
+int write_back(struct MML *mm_in, struct WBL *wb_out){
+	if(mm_in->pc4 == 0xffffffff) return -1;
+	if(mm_in->valid == 0) return 0;
+
+	//WB 단계 printf
+	//printf("[W] inst: 0x%08x, PC: 0x%x\n", mm_in->ir, mm_in->pc4);
+
+	int wb_v;
+	if(mm_in->c.RegWrite){ //R-type, I-type, LW
+		if(mm_in->c.MemtoReg) //LW
+			wb_v = mm_in->mm_output;
+		else //R-type, I-type
+			wb_v = mm_in->ALU_result;
+		Regs[mm_in->WReg] = wb_v;
+		//printf("        R[%d] = 0x%x\n",
+                //	mm_in->WReg, wb_v);
+	}
+
+        wb_out->valid = mm_in->valid;
+        wb_out->ir = mm_in->ir;
+        wb_out->i = mm_in->i;
+        wb_out->c = mm_in->c;
+        wb_out->WReg = mm_in->WReg;
+        wb_out->pc4 = mm_in->pc4;
+
+	wb_out->wb_v = wb_v;
+	wb_out->pc4 = mm_in->pc4;
+
+	return 0;
+}
+
+//cycle 맨 마지막에서 print changed state, count instructions, update pipeline
+void state_and_update_cnt(struct IFL if_latch[2], struct IDL id_latch[2], struct EXL ex_latch[2], struct MML mm_latch[2], struct WBL wb_latch[2]){
+	//prints out the changed state
+	//count instructions
+	printf("\n");
+
+	//changed PC
+	if(id_latch[0].c.Jump){
+                printf("changed PC      : PC = 0x%x\n", PC);
+                j_count++;
+        }
+        if(ex_latch[0].c.Branch){
+                if(ex_latch[0].bcond){
+                	printf("changed PC      : PC = 0x%x\n",
+                        	ex_latch[0].npc);
+                }
+                else nb_count++;
+                b_count++;
+        }
+
+	//changed memory
+        if(mm_latch[0].c.MemWrite || mm_latch[0].c.MemRead){
+		if(mm_latch[0].c.MemWrite)
+        		printf("changed memory  : M[0x%x] = 0x%x\n",
+                		mm_latch[0].ALU_result,Regs[mm_latch[0].i.rt]);
+		m_count++;
+	}
+
+	//changed register
+        if(wb_latch[0].c.RegWrite){
+                printf("changed register: R[%d] = 0x%x\n",
+                	 wb_latch[0].WReg, wb_latch[0].wb_v);
+        }
+
+	r_count = inst_count - m_count - b_count - j_count;
+
+	//update
+	if_latch[1] = if_latch[0];
+	id_latch[1] = id_latch[0];
+	ex_latch[1] = ex_latch[0];
+	mm_latch[1] = mm_latch[0];
+	wb_latch[1] = wb_latch[0];
+}
+
+//모든 cycle이 종료되고 count한 값들 출력
+void print_stastics(){
+	printf("\nfinal return value(R[2])             : %d\n", Regs[2]);
+        printf("# of cycles                          : %d\n", cycle);
+        printf("# of instuctions                     : %d\n", inst_count);
+        printf("# of memory operation instructions   : %d\n", m_count);
+        printf("# of register operation instructions : %d\n", r_count);
+        printf("# of branch instructions             : %d\n", b_count);
+        printf("# of not-taken branches              : %d\n", nb_count);
+        printf("# of jump instructions               : %d\n\n", j_count);
+}
+
+//control type별 구분
+void control(int op, int fun, int c_rs, int c_rt){
+        d_c.RegDst = 0;
+        d_c.Jump = 0;
+        d_c.Branch = 0;
+        d_c.MemRead = 0;
+        d_c.MemtoReg = 0;
+        d_c.ALUOp = 0x100;
+        d_c.MemWrite = 0;
+        d_c.ALUSrc = 0;
+        d_c.RegWrite = 0;
+
+        if(op == 0){
+                if(fun == 0x8 || fun == 0x9) //jr, jalr
+                        d_c.Jump = 1;
+                else{ //R_type
+                        d_c.RegDst = 1;
+                        d_c.ALUOp = fun;
+                        d_c.RegWrite = 1;
+                }
+        }
+        else if(op == 0x2 || op == 0x3) //jal, jr
+                d_c.Jump = 1;
+        else if(op == 0x23){ //lw
+                d_c.MemRead = 1;
+                d_c.MemtoReg = 1;
+                d_c.ALUOp = 0x20;
+                d_c.ALUSrc = 1;
+                d_c.RegWrite = 1;
+        }
+        else if(op == 0x2b){ //sw
+                d_c.ALUOp = 0x20;
+                d_c.MemWrite = 1;
+                d_c.ALUSrc = 1;
+        }
+        else if(op == 0x4 || op == 0x5){ //beq, bne
+                d_c.Branch = 1;
+                d_c.ALUOp = op;
+        }
+        else { //I_type
+                d_c.ALUOp = op;
+                d_c.ALUSrc = 1;
+                d_c.RegWrite = 1;
+        }
+}
